4bcfe3bf34debb71bf9d48a22d79bd8c2044e3d2
Alexandr Peters <alexandr.peters@gmail.com>
2024-10-26 07:20:54 +0600

    Update invoice.php config file

--- config/invoice.php
+++ config/invoice.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    "api_key" => "1526fec01b5d11f4df4f2160627ce351",
-    "login" => "demo",
-    "default_terminal_name" => "Laravel SDK"
+    "api_key" => env('INVOICE_API_KEY', "1526fec01b5d11f4df4f2160627ce351"),
+    "login" => env('INVOICE_LOGIN', "demo"),
+    "default_terminal_name" => env('INVOICE_TERMINAL_NAME', "Laravel SDK"),
 ];
